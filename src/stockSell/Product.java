2bc944dd23ed14e017e79c64ca698fb97ee3007a
Akaporn Katip <brombom43531@gmail.com>
2018-11-10 23:25:57 +0700

    upload tracker files

---
+++ src/stockSell/Product.java
@@ -0,0 +1,144 @@
+package stockSell;
+
+public class Product {
+
+	public String name = ""; // สร้าง name ไว้เก็บชื่อ
+	public float price = 0.0f;// สร้าง price ไว้เก็บราคา
+	public int amount = 0;// สร้าง amount ไว้เก็บจำนวนสินค้า
+	public int length = 0;// เก็บจำนวนของรายการสินค้า
+	public boolean hasOption = false;
+	
+	private Option option;
+	private Product products[];
+	private int maxProduct = 0;
+
+	/**
+	 * method นี้จะเรียกว่า Constructor จะถูกเรียกใช้เมื่อ object ถูกสร้าง
+	 * เมื่อถูกสร้างแล้วจะกำหนดให้ products เก็บค่า array ที่มีสมาชิก maxProduct ตัว
+	 * 
+	 * @param maxProduct คือ ค่าความกว้างของ array เป็นจำนวนเต็ม(int)
+	 */
+	public Product(int maxProduct) {
+		this.maxProduct = maxProduct;
+		products = new Product[this.maxProduct];// สร้าง array ที่มีสมาชิกทั้งหมด maxProduct ตัว
+	}
+
+	public Product() {
+
+	}
+
+	/**
+	 * method นี้ทำหน้าที่เก็บชื่อสินค้า ราคา และ จำนวน โดยใช้ค่า พารามิเตอร์
+	 * จะถูกกำหนดให้กับ name, price, amount แล้วจะนำค่าที่ได้ เก็บไว้ใน อ็อบเจ็ก
+	 * ที่ชื่อ product สุดท้ายก็จะนำไปเก็บไว้ ใน array
+	 * 
+	 * @param name   คือ ชื่อสินค้า
+	 * @param price  คือ ราคาของสินค้า
+	 * @param amount คือ จำนวนของสินค้า
+	 */
+	public void createProduct(String name, float price, int amount) {
+		Product product = new Product();
+		product.name = name;
+		product.price = price;
+		product.amount = amount;
+		products[length] = product;
+
+		length++;
+	}
+
+	public void createProduct(String name, float price, Option option) {
+		Product product = new Product();
+		product.name = name;
+		product.price = price;
+		product.amount = option.getAmount();
+		product.option = option;
+		product.hasOption = true;
+		
+		products[length] = product;
+		length++;
+	}
+
+	public Option getOption() {
+		return option;
+	}
+	/**
+	 * @return Product
+	 */
+	public Product getProductAt(int getProductAt) {
+		return products[getProductAt];
+	}
+
+	/**
+	 * 
+	 * @return Products array
+	 */
+	public Product[] toArray() {
+		return products;
+	}
+
+	/**
+	 * method นี้จะทำหน้าที่ตัดสต็อก
+	 * การที่จะตัดสต็อกได้นั้นจำเป็นต้องรู้ว่าการขายมีอะไรบ้าง ซึ่งก็คือ receipts
+	 * ที่เป็น พารามิเตอร์ ของ method แล้วก็จำนำมาวนลูปรายการที่สั่งซื้อ
+	 * เพื่อตัดสต็อกใน Product
+	 * 
+	 * @param receipts คือ ข้อมูลรายการที่ขายไป
+	 */
+	public void updateInventory(Receipt receipts) {
+		for (int i = 0; i < receipts.length; i++) {
+			receipts.getReceiptAt(i).product.amount -= receipts.getReceiptAt(i).BuyAmount;
+		}
+	}
+}
+
+class Option {
+	String name = "";
+	float addValue = 0;
+	int amount = 0;
+	int length = 0;
+	int maxOption = 0;
+	private Option options[] = null;
+
+	Option(int maxOption) {
+		this.maxOption = maxOption;
+		options = new Option[maxOption];
+	}
+
+	Option() {
+
+	}
+
+	public Option createOption(String name, float addValue, int amount) {
+		Option option = new Option();
+		option.name = name;
+		option.addValue = addValue;
+		option.amount = amount;
+		this.length++;
+		
+		return option;
+	}
+
+	public Option createOptionArray(Option... option) {
+
+		for (int i = 0; i < option.length && (option.length < this.maxOption); i++) {
+			this.options[this.length] = option[i];
+			this.length++;
+		}
+		return this;
+	}
+
+	public Option getOptionAt(int index) {
+		return this.options[index];
+	}
+
+	public Option[] toArray() {
+		return this.options;
+	}
+
+	public int getAmount() {
+		for (int i = 0; i < this.length; i++) {
+			this.amount += options[i].amount;
+		}
+		return this.amount;
+	}
+}
---
+++ src/stockSell/Transaction.java
@@ -0,0 +1,108 @@
+package stockSell;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Transaction {
+
+	Product product = new Product(100);
+	Product shirt = new Product(100);
+	Receipt receipt = new Receipt(100);
+	Scanner getKey = new Scanner(System.in);
+
+	/**
+	 * 
+	 * method นี้จะเรียกว่า Constructor จะถูกเรียกใช้เมื่อ object ถูกสร้าง
+	 * เราเลยนำมาใช้ในการ กำหนดค่าเริ่มต้น ตัวอย่างนี้จะเป็นการ สร้างเมนู
+	 * และกำหนดค่าต่าง ๆ ให้กับเมนู
+	 * 
+	 */
+	Transaction() {
+		product.createProduct("Orange Juice", 20, 20); // ใส่ ชื่อ ราคา และ จำนวน ให้กับพารามิเตอร์
+		product.createProduct("Apple Juice", 10, 20); // ใส่ ชื่อ ราคา และ จำนวน ให้กับพารามิเตอร์
+		product.createProduct("Grape Juice", 15, 20); // ใส่ ชื่อ ราคา และ จำนวน ให้กับพารามิเตอร์
+		product.createProduct("Coconut Water", 15, 20); // ใส่ ชื่อ ราคา และ จำนวน ให้กับพารามิเตอร์
+		product.createProduct("Tomato Juice", 15, 20); // ใส่ ชื่อ ราคา และ จำนวน ให้กับพารามิเตอร์
+		product.createProduct("Avocado Juice", 15, 20); // ใส่ ชื่อ ราคา และ จำนวน ให้กับพารามิเตอร์
+
+		
+		
+		product.createProduct("T-shirt", 230, new Option(100).createOptionArray(new Option().createOption("Orange", 0, 5),
+				new Option().createOption("Black", 5, 5), new Option().createOption("gray", 10, 5))); //เป็นการสร้าง product แบบมี option ให้เลือก
+		
+		
+		
+		viewMenu();// เรียกให้ method viewMenu() เริ่มทำงาน
+	}
+
+	public static void main(String[] args) {
+		new Transaction();
+	}
+
+	public void viewMenu() {
+		System.out.println("id\tName      \t\tprice\tAmount");
+		for (int i = 0; i < product.length; i++) {
+			System.out.println(i + 1 + ".\t" + product.getProductAt(i).name + "\t\t" + product.getProductAt(i).price
+					+ "	" + product.getProductAt(i).amount);
+			
+			for(int x=0;product.getProductAt(i).hasOption&&(x<product.getProductAt(i).getOption().length);x++) {
+				System.out.println("\t-"+product.getProductAt(i).getOption().getOptionAt(x).name+
+											"\t"+product.getProductAt(i).getOption().getOptionAt(x).addValue);
+			}
+		}
+		System.out.println("-----------------------------------------------");
+		setReceipt();
+	}
+
+	public void setReceipt() {
+		int num_product;
+		int amount;
+		int command;
+		boolean runloop = true;
+		while (runloop) {
+			System.out.print("Select juice : ");
+
+			num_product = getKey.nextInt() - 1;
+
+			System.out.print("How many dso you want? : ");
+			amount = getKey.nextInt();
+
+			if (receipt.addProductToReceipt(product.getProductAt(num_product), amount)) {
+
+				System.out.print("1 To Finish Receipt 0 To Continue.");
+
+				command = getKey.nextInt();
+
+				if (command == 1) {
+					runloop = false;
+
+					System.out.println("\n\n\n------------------Receipt------------------");
+					viewReceipt();
+
+					System.out.println("-----------------------------------------------");
+					setInventory();
+					System.out.println("-----------------------------------------------");
+				}
+			} else {
+				System.out.println("cargo not enough.");
+			}
+		}
+	}
+
+	public void viewReceipt() {
+		System.out.println("  \tProduct\t\tAmount*price\tTotal");
+		for (int i = 0; i < receipt.length; i++) {
+
+			System.out.println(i + 1 + ".\t" + receipt.getReceiptAt(i).product.name + "\t"
+					+ receipt.getReceiptAt(i).BuyAmount + "x" + receipt.getReceiptAt(i).product.price + "\t\t"
+					+ receipt.getReceiptAt(i).BuyAmount * receipt.getReceiptAt(i).product.price);
+		}
+		System.out.println("Total : " + receipt.getTotalPrice());
+	}
+
+	public void setInventory() {
+		product.updateInventory(receipt);
+		receipt = new Receipt(100);
+		viewMenu();
+	}
+}
